055b66e0922ff1fedaf751dca573770ee8da3537
KadariKondal <mattipallyswami888@gmail.com>
2024-08-13 22:41:45 +0530

    nativesql practise

--- Hotel_management_system5/src/main/java/com/naresh/Database/Repository/HotelRepository.java
+++ Hotel_management_system5/src/main/java/com/naresh/Database/Repository/HotelRepository.java
@@ -51,7 +51,34 @@ public interface HotelRepository extends JpaRepository<Hotel, Integer> {
 			  public  HotelInfoDataInterfaceProjection findByHotelInfoforJPQLInterfaceBasedProjection(@Param("hotelName") String hotelName);
 		
 		
+		//   complete native sql joins with different senarios??
+
+     //Retrieve All Hotels with Their Rooms
+			
+		@Query(value="select h.*,r.room_id,r.price from hotel h left Join room r on h.hotel_Id=r.hotel_id",nativeQuery = true)
+			
+			public List<Hotel> getAllHotelWithTheirRooms();
 		
+		 
+		
+		
+		
+		
+		
+		//2. Retrieve Hotels Without Rooms	
+		 
+		@Query(value="select h.* from hotel h left join room r on h.hotel_id=r.hotel_id where r.Hotel_Id is Null",nativeQuery = true)
+		public List<Hotel> getHotelsWithoutRooms();
+			
+			
+			// 4. Count Rooms for Each Hotel
+		
+
+		@Query(value="select h.hotel_name,count(r.room_id) as no_of_rooms from hotel h left join room r on h.hotel_id=r.hotel_id group by h.hotel_name",nativeQuery = true)
+		
+		public  List<Object[]> getHotelsWithNoOfRooms();
+			
+			
 			
 			
 			
